90cfc6931b7910ca61a14e27b53438a0e55ebd2e
Adriano Ibanez <dricoibanez@gmail.com>
2024-12-17 19:25:27 -0300

    Criando o callenge literalura

---
+++ Challenge-Literalura/src/main/java/br/alura/cruso/Challenge/Literalura/ChallengeLiteraluraApplication.java
@@ -0,0 +1,15 @@
+package br.alura.cruso.Challenge.Literalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeLiteraluraApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeLiteraluraApplication.class, args);
+	}
+
+    @Override
+
+}
