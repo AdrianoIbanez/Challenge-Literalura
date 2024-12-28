767617c437437c2c5b80a3c7b9e09c8f4971e1c5
Adriano Ibanez <dricoibanez@gmail.com>
2024-12-27 21:01:39 -0300

    Criando a classe Models/livros

--- Challenge-Literalura/src/main/java/models/Livro.java
+++ Challenge-Literalura/src/main/java/models/Livro.java
@@ -1,12 +1,30 @@
 package models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
-@Table
+@Table(name = "livros")
 @JsonIgnoreProperties(ignoreUnknown = true)
 
 public class Livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String titulo;
+
+    @ManyToOne(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+    private Autor autor;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "livros_idiomas",
+            joinColumns = @JoinColumn(name = "livro_id"),
+            inverseJoinColumns = @JoinColumn(name = "idioma_id" ))
+
+
+
+
 }
